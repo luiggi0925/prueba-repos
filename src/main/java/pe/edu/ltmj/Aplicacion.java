182df32dc90a83083fdc307f1e9d5e3aba54998e
Luiggi Mendoza <luiggi0925@gmail.com>
2020-04-29 19:31:43 -0500

    Agregando saludos

--- src/main/java/pe/edu/ltmj/Aplicacion.java
+++ src/main/java/pe/edu/ltmj/Aplicacion.java
@@ -4,5 +4,6 @@ public class Aplicacion {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		new Saludo().saludar("Luiggi");
 	}
 }
---
+++ src/main/java/pe/edu/ltmj/Saludo.java
@@ -0,0 +1,8 @@
+package pe.edu.ltmj;
+
+public class Saludo {
+
+	public void saludar(String nombre) {
+		System.out.println("hola " + nombre);
+	}
+}
